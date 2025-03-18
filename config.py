c2bc2582ea8d78cb1820f9df6560dfd39c58fcac
mondayinred <finegu06@gmail.com>
2025-03-18 18:46:16 +0900

    패치 데이터셋으로 학습시키기 전

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ train_config = {
     #     transforms.Resize((input_config['input_size'][0] / input_config['sampling_factor'],  input_config['input_size'][1] / input_config['sampling_factor'])), # sampling_factor만큼 downsampling
     # ]),
     
-    'train_data_path' : "/home/lab/Datasets/UCSR_Datasets/Train/LR_HIFI/high",
+    'train_data_path' : "/home/lab/Datasets/UCSR_Datasets/Train/HR",
     'train_ratio' : 0.8,
     'crop_size' : (96, 96),
     'srresnet_save_path' : '/home/lab/work/SRGAN/SRResnet_parameters',
---
+++ preprocessing.py
@@ -0,0 +1,28 @@
+import os
+from PIL import Image
+
+from config import train_config
+
+# 이미지 로드
+image_src_path = '/home/lab/Datasets/UCSR_Datasets/Train/HR'
+image_save_path = '/home/lab/Datasets/SRGAN_data/Train_patches'
+patch_size = train_config['crop_size']
+
+patch_idx = 1  # 패치 번호
+for f in os.listdir(image_src_path):
+    image_path = os.path.join(image_src_path, f)
+    image = Image.open(image_path)
+    image = image.convert("RGB")  # RGB 변환 (필요한 경우)
+
+    width, height = image.size
+    patch_w, patch_h = patch_size
+
+    for y in range(0, height, patch_h):
+        for x in range(0, width, patch_w):
+            # 현재 패치 영역이 이미지 범위를 넘어가지 않도록 함
+            if x + patch_w <= width and y + patch_h <= height:
+                patch = image.crop((x, y, x + patch_w, y + patch_h))
+                patch.save(os.path.join(image_save_path, f"patch_{patch_idx}.png"))
+                patch_idx += 1
+
+    print(f"총 {patch_idx}개의 패치를 저장했습니다.")
